ff035ea947341be6a5f1385091342042a7dd9cae
Shradha <shradha.sgl@gmail.com>
2020-01-17 20:47:14 +0530

    Board printed

--- global_funct.py
+++ global_funct.py
@@ -5,7 +5,7 @@ import global_var
 from colorama import Fore, Back, Style
 
 def create_header():
-    print("\033[2;1H" + Fore.WHITE + Back.BLUE + Style.BRIGHT + ("SCORE: " + str(global_var.mando.score()) + "   |  COINS: " + str(global_var.mando.coins()) + "   |  LIVES: " + str(global_var.mando.lives()) + "   |  TIME: " +str(global_var.TIME_REM)  .center(config.columns), end='')
+    print("\033[2;1H" + Fore.WHITE + Back.BLUE + Style.BRIGHT + ("SCORE: " + str(global_var.mando.score()) + "   |  COINS: " + str(global_var.mando.coins()) + "   |  LIVES: " + str(global_var.mando.lives()) + "   |  TIME: " +str(global_var.TIME_REM))  .center(config.columns), end='')
     print(Style.RESET_ALL)
 
 def print_board():
