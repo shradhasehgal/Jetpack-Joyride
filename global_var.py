4a27380a7330d2cfaea4cb33244a49d567eb5644
Shradha <shradha.sgl@gmail.com>
2020-01-17 21:33:58 +0530

    Board moved

--- global_var.py
+++ global_var.py
@@ -5,7 +5,8 @@ import config
 mp = board.Map()
 
 TIME_REM = 0
-COINS = 0
-SCORE = 0
+TIME_BEGUN = 0
+TIME_TOTAL = 100
+LAST_TIME = 0
 
 mando = objects.Mando(config.mando, 0, mp.height - len(config.mando) - 1, config.lives)
--- main.py
+++ main.py
@@ -3,7 +3,23 @@ import global_funct
 import random
 import config
 import objects
+from time import time 
 
 global_funct.create_board()
 global_funct.print_board()
 
+global_var.LAST_TIME = time()
+global_var.TIME_BEGUN = round(global_var.LAST_TIME)
+
+while True:
+
+    global_var.TIME_REM = global_var.TIME_TOTAL - round(time()) + global_var.TIME_BEGUN
+
+    if global_var.TIME_REM == 0:
+        print("Time over!")
+        break
+    
+    # if time() - global_var.LAST_TIME > 0.04:
+    global_var.mp.start_index += 1
+    global_funct.print_board()
+    global_var.LAST_TIME = time()
